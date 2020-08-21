250d40ab4f73537b26d806a804ba72275ed7789f
Cerem Cem ASLAN <cem@aktos.io>
2020-08-22 01:45:53 +0300

    disregards small movements, which fixes #2

--- on-idle.sh
+++ on-idle.sh
@@ -7,11 +7,19 @@ safe_source () { [[ ! -z ${1:-} ]] && source $1; _dir="$(cd "$(dirname "${BASH_S
 # $0 10 command
 
 idle=false
-idleAfter=$(($1 * 1000))
+idleAfter=$1
+idleAfterMs=$(($idleAfter * 1000))
 shift
 
 log(){
-    echo "`date +"%d.%m.%Y %H:%M"`  : $@"
+    echo "`date +"%d.%m.%Y %H:%M:%S"`  : $@"
+}
+
+function format_seconds() {
+  (($1 >= 86400)) && printf '%d days and ' $(($1 / 86400)) # days
+  (($1 >= 3600)) && printf '%02d:' $(($1 / 3600 % 24))     # hours
+  (($1 >= 60)) && printf '%02d:' $(($1 / 60 % 60))         # minutes
+  printf '%02d%s\n' $(($1 % 60)) "$( (($1 < 60 )) && echo ' s.' || echo '')"
 }
 
 exe_pid=
@@ -31,19 +39,33 @@ while :; do
            }
 done
 
-log "Started idle watchdog."
+idleBase=0 # milliseconds, the offset
+idleTimeMsNew=
+log "Started idle watchdog. Timeout is: $(format_seconds $idleAfter)"
 while true; do
-  idleTimeMillis=$($_sdir/getIdle)
+  idleTimeMsNew=$($_sdir/getIdle)
+  if [[ -n ${idleTimeMs:-} ]]; then 
+    if [[ $idle = false && $idleTimeMsNew -lt $idleTimeMs ]]; then
+      sleep 1
+      if [[ $($_sdir/getIdle) -gt 500 ]]; then 
+        idleBase=$(($idleBase + $idleTimeMs))
+        log "!!! adding offset by ${idleTimeMs}ms, total: $(($idleBase / 1000))s"
+      else
+        idleBase=0
+      fi
+    fi 
+  fi 
+  idleTimeMs=$idleTimeMsNew
   #echo $idleTimeMillis  # just for debug purposes.
-  if [[ $idle = false && $idleTimeMillis -gt $idleAfter ]] ; then
-    log "Computer is now idle."   # or whatever command(s) you want to run...
+  if [[ $idle = false && $(($idleTimeMs + $idleBase)) -gt $idleAfterMs ]] ; then
+    log "Computer is now idle. (after $(format_seconds $idleAfter))"   # or whatever command(s) you want to run...
     "$@" & exe_pid=$!
     wait
     exe_pid=
     idle=true
   fi
 
-  if [[ $idle = true && $idleTimeMillis -lt $idleAfter ]] ; then
+  if [[ $idle = true && $(($idleTimeMs + $idleBase)) -lt $idleAfterMs ]] ; then
     log "end idle"     # same here.
     idle=false
   fi
